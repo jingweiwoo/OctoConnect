a5c0782bb42ae88f6a2db90ce86a9e6cbd95eeaf
jingweiwu <jingwei.wu1983@gmail.com>
2016-07-26 23:12:57 +0800

    initial project commit

---
+++ OctoConnect/OctoConnect.h
@@ -0,0 +1,19 @@
+//
+//  OctoConnect.h
+//  OctoConnect
+//
+//  Created by Jingwei Wu on 7/17/16.
+//  Copyright © 2016 jingweiwu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OctoConnect.
+FOUNDATION_EXPORT double OctoConnectVersionNumber;
+
+//! Project version string for OctoConnect.
+FOUNDATION_EXPORT const unsigned char OctoConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OctoConnect/PublicHeader.h>
+
+
